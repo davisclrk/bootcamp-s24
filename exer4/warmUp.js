ef727d9b17ac7620ab668ab8074b76c0e64f9a55
davisclrk <dclark304@gatech.edu>
2024-07-23 19:18:32 -0400

    solved exercise 4

--- exer4/warmUp.js
+++ exer4/warmUp.js
@@ -16,7 +16,12 @@
  * F = anything < 60
  */
 function toLetterGrade(numGrade) {
-  // TODO
+  if (numGrade > 100 || numGrade < 0) return "INVALID";
+  else if (numGrade >= 90) return "A";
+  else if (numGrade >= 80) return "B";
+  else if (numGrade >= 70) return "C";
+  else if (numGrade >= 60) return "D";
+  else return "F";
 }
 
 /**
@@ -27,7 +32,7 @@ function toLetterGrade(numGrade) {
  * Example: [9, 3, 4, 1, 2, 0] --> [4, 2, 0]
  */
 function getEvenElements(array) {
-  // TODO
+  return [...array].filter(num => num % 2 == 0);
 }
 
 /**
@@ -40,7 +45,10 @@ function getEvenElements(array) {
  * Example: "I love Bits of Good" --> 4
  */
 function findLongestWord(string) {
-  // TODO
+  const arr = string.split(" ");
+  let longest = 0;
+  arr.forEach(i => longest = i.length > longest ? i.length : longest);
+  return longest;
 }
 
 /**
@@ -65,7 +73,7 @@ function findLongestWord(string) {
     } 
  */
 function combineObjects(object1, object2) {
-  // TODO
+  return {...object1, ...object2}
 }
 
 /**
@@ -77,7 +85,9 @@ function combineObjects(object1, object2) {
  *  Example: [1, 2, 3] -> [3, 2, 1]
  */
 function reverseArr(array) {
-  // TODO
+  const ans = [];
+  for (let i=array.length-1;i>=0;i--) ans.push(array[i]);
+  return ans;
 }
 
 // DO NOT EDIT BELOW THIS LINE -- the code is for testing purposes only!
